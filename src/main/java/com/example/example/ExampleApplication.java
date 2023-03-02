36cfcbd9b6b3f63ef5bd6e1e11a20ea6daadfded
Depar3 <parkmin0318@gmail.com>
2023-03-02 14:54:16 +0900

    updated

---
+++ src/main/java/com/example/example/ExampleApplication.java
@@ -0,0 +1,24 @@
+package com.example.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
+
+@RestController
+class example {
+
+	@GetMapping("/")
+	public String hello () {
+		return "Hello World!!";
+	}
+}
